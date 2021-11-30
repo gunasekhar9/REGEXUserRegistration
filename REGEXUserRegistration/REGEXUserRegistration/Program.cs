e71730dd80caabb3d60c66c70b17755bcbef812a
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-30 14:40:16 +0530

    [gunasekhar9] add : REGEXUserRegestration

---
+++ REGEXUserRegistration/REGEXUserRegistration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace REGEXUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to REGEX User Registration");
+            UserRegistration validation = new UserRegistration();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to the User Registration validation program");
+                Console.WriteLine("Enter the validation to Execute : \n 1. Candidate First Name \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(validation.ValidateFirstName("Gunasekhar"));
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
